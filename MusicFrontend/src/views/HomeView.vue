d70535c46e2648e8c7013570a1b97f4dcd4ba910
Pepijn <437675@student.fontys.nl>
2024-09-01 22:32:17 +0200

    update frontend to work with base64 string audio

--- MusicFrontend/src/views/HomeView.vue
+++ MusicFrontend/src/views/HomeView.vue
@@ -1,16 +1,26 @@
 <script>
 export default{
   methods:{
-      playSound (sound) {
+      async playSound (url) {
+        const axios = require('axios');
+        const res = await fetch(url)
+        const data = await res.json()
+        var sound;
+        if (data.audioData != 0) {
+          sound = audioData
+        }else{
+          console.log(data)
+        }
         if(sound) {
-          var audio = new Audio(sound);
+          
+          var audio = new Audio('data:audio/mp3;base64,'+sound);
           audio.play();
         }
       }
     },
     data() { 
         return { 
-          url: "https://commondatastorage.googleapis.com/codeskulptor-assets/week7-bounce.m4a"
+          url: "https://localhost:7002/audiofiles"
         }
       }
   }
@@ -19,9 +29,8 @@ export default{
 
 <template>
   <main>
-    <input type="text" v-model="this.url"> </input>
+    <input type="text" v-model="this.url" />
     <p>url: {{ this.url }}</p>
     <button class="btn btn-primary btn-sm" @click="playSound(this.url)"><span class="fa fa-play-circle-o"></span>play sound</button>
-  
   </main>
 </template>
