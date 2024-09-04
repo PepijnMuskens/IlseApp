c0f8dc90a5dc8232661121e2196bdb989705305c
Pepijn <437675@student.fontys.nl>
2024-09-04 09:13:13 +0200

    removed backend

--- Musiq_Quiz/MusicQuiz/DTOs/SongNameDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MusicQuiz.DTOs
-{
-    public class SongNameDTO
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public int FileId { get; set; }
-    }
-}
